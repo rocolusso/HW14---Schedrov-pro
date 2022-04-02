d9f131da5892e49b228b03016d193bffe268e6f1
schedrov <rocolusso@icloud.com>
2022-04-02 18:50:07 +0300

    updated

--- js/task1.js
+++ js/task1.js
@@ -14,23 +14,39 @@
 // 10; // false
 
 
-const num = +prompt('Введите число');
+let num = +prompt('Введите число');
 
-let i =2;
+let deliteli = [];
+let max =num/2;
 
-while (i<=Math.sqrt(num)){
+if (num % 1 === 0) {
 
-   if (num % i ===0) {
-       console.log(`${num} // false`);
-       break;
-   } else {
-       console.log(`${num} // true`)
-       break;
-   }
-    i++ ;
+    deliteli.push(1);
+    num =Math.round(num / 1);
+
+    for (let i=2; i<= max; i++) {
+        if (num % i === 0) {
+            deliteli.push(1 * i);
+        }
+    }
+    deliteli.push(1 * num);
 }
 
+console.log(deliteli);    // Вывод количества делителей введеного числа
 
+if (deliteli[0] !== num){
+// Обертка - это проверка на 1 , если не сделать - выведет для единицы 2 делителя, а
+//     по условию задачи 1 - это false
+//
+    if (deliteli.length>2  ){
+        console.log(`${num} // false`);
+    } else {
+        console.log(`${num} // true`);
+    }
+
+} else {
+    console.log(`${num} // false`);
+}
 
 
 
